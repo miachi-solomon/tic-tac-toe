3dede7031ccb993034c48ee6927f217442b869f0
Miachi Solomon <smiachi10@gmail.com>
2025-07-16 23:51:28 +0100

    creating 2D array

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const Gameboard = {
     gameboard: []
-}
+};
 
 // create a factory function that returns a player object with a method and property.
-function player (name) {
-    let symbol;
+const player = (name) => {
+    let symbol = [];
     return {
         name,
         symbol,
         setSymbol: function (sym) {
-            symbol = sym;
+            symbol.push(sym);
         }
     }
-}
+};
+
